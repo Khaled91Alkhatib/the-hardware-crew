219837748c805f968043f16c18d3ab35d41405fe
Khaled Alkhatib <khaledkhatib91@hotmail.com>
2022-11-17 15:05:09 -0500

    Add exports

---
+++ src/components/index.js
@@ -0,0 +1,4 @@
+export { default as Homepage } from "./Homepage/Homepage";
+export { default as Navbar } from "./Navbar/Navbar"
+export { default as AllProducts } from "./AllProducts/AllProducts"
+export { default as Product } from "./Product/Product"
