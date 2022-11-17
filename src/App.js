3f28579345346a63046522e1b4f302b5cdc30ace
Khaled Alkhatib <khaledkhatib91@hotmail.com>
2022-11-17 15:02:25 -0500

    make an axios request to api and add data to context and create routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import GeneralContext from "./contexts/GeneralContext";
+import { Homepage, Navbar, AllProducts } from "./components/index";
 
 function App() {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    axios.get('http://localhost:5001/api/products')
+      .then((res) => {
+        setProducts(prev => res.data.products);
+        // console.log(res.data.products);
+      });
+  }, []);
+
   return (
     <div>
-      khaled
+      <GeneralContext.Provider value={{ products }} >
+        <BrowserRouter>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Homepage />} />
+            <Route path="/products/:id" element={<AllProducts />} />
+          </Routes>
+        </BrowserRouter>
+      </GeneralContext.Provider>
     </div>
   );
 }
