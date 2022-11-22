97b98f55ffee2fe31662c9d74825164fde3cf206
Khaled Alkhatib <khaledkhatib91@hotmail.com>
2022-11-21 21:53:44 -0500

    Add new export

--- src/components/index.js
+++ src/components/index.js
@@ -2,3 +2,4 @@ export { default as Homepage } from "./Homepage/Homepage";
 export { default as Navbar } from "./Navbar/Navbar"
 export { default as AllProducts } from "./AllProducts/AllProducts"
 export { default as Product } from "./Product/Product"
+export { default as SingleProduct } from ".//SingleProduct/SingleProduct"
