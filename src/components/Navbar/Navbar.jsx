1a5d01e1eb6a9d16a1c8a2a518166f3758546cc1
Khaled Alkhatib <khaledkhatib91@hotmail.com>
2022-11-24 18:58:43 -0500

    Create a navLink to shoppingCart

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { NavLink, useNavigate } from 'react-router-dom';
 import logo from '../../site-images/logo.png';
+import GeneralContext from '../../contexts/GeneralContext';
 
 import './Navbar.scss';
 
@@ -11,6 +12,7 @@ library.add(faCartShopping);
 
 
 const Navbar = () => {
+  const { cart } = useContext(GeneralContext);
   const navigate = useNavigate();
   const clickLogo = () => {
     navigate('/');
@@ -20,12 +22,12 @@ const Navbar = () => {
     <div className='nav-buttons'>
       <img onClick={clickLogo} src={logo} alt='logo' />
       <div>
-        <button><NavLink to="/products/keyboards">Keyboards</NavLink></button>
-        <button><NavLink to="/products/mice">Mice</NavLink></button>
-        <button><NavLink to="/products/headsets">Headsets</NavLink></button>
+        <button><NavLink className='collection' to="/products/keyboards">Keyboards</NavLink></button>
+        <button><NavLink className='collection' to="/products/mice">Mice</NavLink></button>
+        <button><NavLink className='collection' to="/products/headsets">Headsets</NavLink></button>
       </div>
       <div>
-        <button className='cart'><FontAwesomeIcon icon="fa-solid fa-cart-shopping" size='lg' /><span className='badge'>0</span></button>
+        <button className='cart'><NavLink to="/shoppingcart"><FontAwesomeIcon icon="fa-solid fa-cart-shopping" size='lg' /><span className='badge'>{cart.length}</span></NavLink></button>
       </div>
     </div>
   );
