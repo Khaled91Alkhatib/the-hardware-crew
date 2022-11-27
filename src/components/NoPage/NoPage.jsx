db2a618589a9e25f25861e508ca338aa9f14681e
Khaled Alkhatib <khaledkhatib91@hotmail.com>
2022-11-27 11:16:34 -0500

    Add new component and apply styles

---
+++ src/components/NoPage/NoPage.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import './NoPage.scss'
+
+const NoPage = () => {
+  return (
+    <div className='no-page'>
+      OOPS!
+      Something Went Wrong!<br />
+      Make sure you are at the correct URL.
+      </div>
+  )
+}
+
+export default NoPage
