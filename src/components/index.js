70e803c5db1aa6d9193f4398e940781990f50da6
Khaled Alkhatib <khaledkhatib91@hotmail.com>
2022-11-27 11:15:08 -0500

    Export new route

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,7 @@
 export { default as Homepage } from "./Homepage/Homepage";
-export { default as Navbar } from "./Navbar/Navbar"
-export { default as AllProducts } from "./AllProducts/AllProducts"
-export { default as Product } from "./Product/Product"
-export { default as SingleProduct } from "./SingleProduct/SingleProduct"
-export { default as ShoppingCart } from "./ShoppingCart/ShoppingCart"
+export { default as Navbar } from "./Navbar/Navbar";
+export { default as AllProducts } from "./AllProducts/AllProducts";
+export { default as Product } from "./Product/Product";
+export { default as SingleProduct } from "./SingleProduct/SingleProduct";
+export { default as ShoppingCart } from "./ShoppingCart/ShoppingCart";
+export { default as NoPage } from "./NoPage/NoPage";
