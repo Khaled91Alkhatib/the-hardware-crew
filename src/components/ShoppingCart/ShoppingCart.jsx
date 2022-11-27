3edd31fcd9892e8263fc7a6133ea67c2f5c3d7dc
Khaled Alkhatib <khaledkhatib91@hotmail.com>
2022-11-27 11:18:15 -0500

    Apply conditional depending on whether the cart is empty or not

--- src/components/ShoppingCart/ShoppingCart.jsx
+++ src/components/ShoppingCart/ShoppingCart.jsx
@@ -32,96 +32,106 @@ const ShoppingCart = () => {
     <div className='shopping-cart'>
       <div className='cart-container'>
         <div className='cart-title'>SHOPPING CART</div>
-        <div className='cart-count'>
-          {cartItemNumber === 1 ? "1 Item" : `${cartItemNumber} Items`}
-        </div>
-        <table>
-          <thead>
-            <tr className='headers'>
-              <th style={{ width: "70%" }}>Item Description</th>
-              <th style={{ textAlign: 'center', padding: '8px' }}>Quantity</th>
-              <th style={{ textAlign: 'center', padding: '8px' }}>Price</th>
-            </tr>
-          </thead>
-          <tbody>
-            {cart.map((item, index) => (
-              <tr className='data-in-table' key={index}>
-                <td>
-                  <div className='image-to-desc'>
-                    <div><img className='image-in-cart' src={item.image1} alt='item pic' /></div>
-                    <div className='name-color'>
-                      <div className='item-name'>{item.name}</div>
-                      <div style={{ color: "rgb(200,200,200)" }}>Color: {item.color}</div>
-                      <button
-                        className='remove-button'
-                        onClick={() => onRemoveClick(item.sku)}>
-                        <FontAwesomeIcon icon="fa-solid fa-trash-can" style={{ paddingRight: "5px" }} />
-                        Remove Item
-                      </button>
-                    </div>
-                  </div>
-                </td>
-                <td style={{ textAlign: 'center' }}>
-                  <div className='minus-quantity-plus'>
-                    <button
-                      className='plus-minus-button'
-                      onClick={() => {
-                        if (item.quantity > 1) {
-                          const newCart = cart.map((row) => {
-                            if (row.sku === item.sku) {
-                              return {
-                                ...row,
-                                quantity: item.quantity - 1
-                              };
-                            } else {
-                              return { ...row };
+        {cart.length === 0 ?
+          <div style={{ textAlign: 'center', paddingBottom: '20px', color: 'green' }}>
+            THERE ARE NO ITEMS IN YOUR CART!< br />
+            CHECK OUT OUR COLLECTION!
+          </div> :
+          <>
+            <div className='cart-count'>
+              {cartItemNumber === 1 ? "1 Item" : `${cartItemNumber} Items`}
+            </div>
+            <table>
+              <thead>
+                <tr className='headers'>
+                  <th style={{ width: "70%" }}>Item Description</th>
+                  <th style={{ textAlign: 'center', padding: '8px' }}>Quantity</th>
+                  <th style={{ textAlign: 'center', padding: '8px' }}>Price</th>
+                </tr>
+              </thead>
+              <tbody>
+                {cart.map((item, index) => (
+                  <tr className='data-in-table' key={index}>
+                    <td>
+                      <div className='image-to-desc'>
+                        <div><img className='image-in-cart' src={item.image1} alt='item pic' /></div>
+                        <div className='name-color'>
+                          <div className='item-name'>{item.name}</div>
+                          <div style={{ color: "rgb(200,200,200)" }}>Color: {item.color}</div>
+                          <button
+                            className='remove-button'
+                            onClick={() => onRemoveClick(item.sku)}>
+                            <FontAwesomeIcon icon="fa-solid fa-trash-can" style={{ paddingRight: "5px" }} />
+                            Remove Item
+                          </button>
+                        </div>
+                      </div>
+                    </td>
+                    <td style={{ textAlign: 'center' }}>
+                      <div className='minus-quantity-plus'>
+                        <button
+                          className='plus-minus-button'
+                          onClick={() => {
+                            if (item.quantity > 1) {
+                              const newCart = cart.map((row) => {
+                                if (row.sku === item.sku) {
+                                  return {
+                                    ...row,
+                                    quantity: item.quantity - 1
+                                  };
+                                } else {
+                                  return { ...row };
+                                }
+                              });
+                              setCart(newCart);
                             }
-                          });
-                          setCart(newCart);
-                        }
-                      }}
-                    >
-                      <FontAwesomeIcon icon="fa-solid fa-square-minus" />
-                    </button>
-                    <div>{item.quantity}</div>
-                    <button
-                      className='plus-minus-button'
-                      onClick={() => {
-                        const newCart = cart.map((row) => {
-                          if (row.sku === item.sku) {
-                            return {
-                              ...row,
-                              quantity: item.quantity + 1
-                            };
-                          } else {
-                            return { ...row };
-                          }
-                        });
-                        setCart(newCart);
-                      }}
-                    >
-                      <FontAwesomeIcon icon="fa-solid fa-square-plus" />
-                    </button>
-                  </div>
-                </td>
-                <td style={{ textAlign: 'center' }}>
-                  ${((item.price * item.quantity) / 100).toFixed(2)}
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-        <div className='taxes-total'>
-          <div className='taxes'>
-            <div>Taxes(13%): </div>
-            <div>${taxes.toFixed(2)}</div>
-          </div>
-          <div className='total'>
-            <div>Total: </div>
-            <div>${totalAftertaxes}</div>
-          </div>
-          <PayButton cart={cart} setCart={setCart} />
-        </div>
+                          }}
+                        >
+                          <FontAwesomeIcon icon="fa-solid fa-square-minus" />
+                        </button>
+                        <div>{item.quantity}</div>
+                        <button
+                          className='plus-minus-button'
+                          onClick={() => {
+                            const newCart = cart.map((row) => {
+                              if (row.sku === item.sku) {
+                                return {
+                                  ...row,
+                                  quantity: item.quantity + 1
+                                };
+                              } else {
+                                return { ...row };
+                              }
+                            });
+                            setCart(newCart);
+                          }}
+                        >
+                          <FontAwesomeIcon icon="fa-solid fa-square-plus" />
+                        </button>
+                      </div>
+                    </td>
+                    <td style={{ textAlign: 'center' }}>
+                      ${((item.price * item.quantity) / 100).toFixed(2)}
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+            <div className='taxes-total'>
+              <div className='taxes'>
+                <div>Taxes(13%): </div>
+                <div>${taxes.toFixed(2)}</div>
+              </div>
+              <div className='total'>
+                <div>Total: </div>
+                <div>${totalAftertaxes}</div>
+              </div>
+              <div className='pay-button'>
+                <PayButton cart={cart} setCart={setCart} />
+              </div>
+            </div>
+          </>
+        }
       </div>
     </div>
   );
