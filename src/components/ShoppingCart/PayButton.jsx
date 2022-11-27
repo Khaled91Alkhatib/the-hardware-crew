c748ae83752b7d8883b43b21c7a7537e6fc434f9
Khaled Alkhatib <khaledkhatib91@hotmail.com>
2022-11-26 22:13:30 -0500

    Create CHECKOUT button functionality through stripe

---
+++ src/components/ShoppingCart/PayButton.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import axios from 'axios';
+// import {url} from "../slice/api"
+
+const payButton = ({ cart, setCart }) => {
+  // async function handleCheckout() {
+  //   let response;
+  //   try {
+  //     response = await axios.post("http://localhost:5001/api/stripe/create-checkout-session", { cart });
+  //   } catch (error) {
+  //     console.log(error);
+  //   }
+  //   if (response.data.url) {
+  //     window.location.href = response.data.url;
+  //   }
+  //   if (response.data.status === 'success') {
+  //     setCart([])
+  //   }
+  // }
+  async function handleCheckout() {
+    await axios.post("http://localhost:5001/api/stripe/create-checkout-session", { cart, setCart })
+      .then((res) => {
+        console.log("pay", res);
+        if (res.data.url) {
+          window.location.href = res.data.url;
+        }
+      }).catch((err) => console.log("error", err.message));
+  };
+
+  return (
+    <>
+      <button onClick={() => handleCheckout()}>
+        CHECKOUT
+      </button>
+    </>
+  );
+};
+
+export default payButton;
