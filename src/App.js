cdd8218b1ce83288b0447f0813f7ecc6dfcf1a78
Khaled Alkhatib <khaledkhatib91@hotmail.com>
2022-11-29 22:37:46 -0500

    Add new route

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import axios from "axios";
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import GeneralContext from "./contexts/GeneralContext";
-import { Homepage, Navbar, AllProducts, SingleProduct, ShoppingCart, NoPage } from "./components/index";
+import { Homepage, Navbar, AllProducts, SingleProduct, ShoppingCart, NoPage, AddProducts } from "./components/index";
 import ThankYou from "./components/ShoppingCart/ThankYou";
 
 function App() {
@@ -43,6 +43,7 @@ function App() {
             <Route path="/shoppingcart" element={<ShoppingCart />} />
             <Route path="/thankyou" element={<ThankYou />} />
             <Route path="/*" element={<NoPage />} />
+            <Route path="/addproducts" element={<AddProducts />} />
           </Routes>
         </BrowserRouter>
       </GeneralContext.Provider>
