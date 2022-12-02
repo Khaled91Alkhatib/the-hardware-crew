aa66e7aa7f7de458940ac2da4ca409dfefe0ebdb
Khaled Alkhatib <khaledkhatib91@hotmail.com>
2022-12-01 19:39:21 -0500

    Comment out axios and use it in app.js

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -1,17 +1,36 @@
-import React, { useState } from 'react';
-import axios from 'axios';
+import React from 'react';
+// import { useNavigate } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+// import axios from 'axios';
 import './Dashboard.scss';
 
-const Dashboard = () => {
-  const [registerUsername, setRegisterUsername] = useState("");
-  const [registerPassword, setRegisterPassword] = useState("");
+const Dashboard = ({ login, register, setRegisterUsername, setRegisterPassword, setUsername, setPassword }) => {
+  // const navigate = useNavigate();
+  // const [registerUsername, setRegisterUsername] = useState("");
+  // const [registerPassword, setRegisterPassword] = useState("");
 
-  const register = () => {
-    axios.post('http://localhost:5001/users', { username: registerUsername, password: registerPassword })
-      .then((res) => {
-        console.log(res);
-      });
-  };
+  // const [username, setUsername] = useState("");
+  // const [password, setPassword] = useState("");
+
+  // const register = () => {
+  //   axios.post('http://localhost:5001/users', { username: registerUsername, password: registerPassword })
+  //     .then((res) => {
+  //       console.log(res);
+  //     });
+  // };
+
+  // const login = () => {
+  //   axios.post('http://localhost:5001/users/login', { username: username, password: password })
+  //     .then((res) => {
+  //       console.log(res);
+  //       if (res.data.rows.length === 0) {
+  //         toast("Invalid Credentials!", { position: "top-right", type: 'success', autoClose: 1500, theme: 'dark' });
+  //       } else {
+  //         navigate("./AddProducts");
+  //       }
+  //     });
+  // };
+
 
 
   return (
@@ -40,14 +59,15 @@ const Dashboard = () => {
         <br />
         <div className='label-input'>
           <label>Username:</label>
-          <input type="text" placeholder="Username" />
+          <input onChange={e => { setUsername(e.target.value); }} type="text" placeholder="Username" />
         </div>
         <div className='label-input'>
           <label>Password:</label>
-          <input type="password" placeholder="Password" />
+          <input onChange={e => { setPassword(e.target.value); }} type="password" placeholder="Password" />
         </div>
         <div className='form-button'>
-          <button className='login'>Log in</button>
+          <button onClick={login} className='login'>Log in</button>
+          <ToastContainer />
         </div>
       </div>
 
