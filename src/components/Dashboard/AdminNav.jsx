99f6dd75cc544aaa3670c13820376cbb04d0f47f
Khaled Alkhatib <khaledkhatib91@hotmail.com>
2022-12-02 22:12:19 -0500

    Create a logout button

--- src/components/Dashboard/AdminNav.jsx
+++ src/components/Dashboard/AdminNav.jsx
@@ -1,5 +1,11 @@
 import React from 'react'
-import { useNavigate } from 'react-router-dom'
+import { NavLink, useNavigate } from 'react-router-dom'
+import logo from '../../site-images/logo.png';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faDoorOpen } from '@fortawesome/free-solid-svg-icons';
+library.add(faDoorOpen);
 
 const AdminNav = ({ user, setUser }) => {
   const navigate = useNavigate()
@@ -10,10 +16,21 @@ const AdminNav = ({ user, setUser }) => {
   }
 
   return (
-    <>
-    <div>AdminNav</div>
-    <button onClick={logout}>Logout</button>
-    </>
+    <div className='nav-buttons'>
+    <img src={logo} alt='logo' />
+    <div>
+      <button><NavLink className='collection' to="/dashboard/addproducts">Add Products</NavLink></button>
+      <button><NavLink className='collection' to="/dashboard/editproducts">Edit Products</NavLink></button>
+      {/* <button><NavLink className='collection' to="/products/headsets">Headsets</NavLink></button> */}
+    </div>
+    <div>
+      <button onClick={logout} className='cart'><NavLink to="/shoppingcart"><FontAwesomeIcon icon="fa-solid fa-door-open" size='lg' color='red' /><span style={{fontSize:'13px'}}> Log Out</span></NavLink></button>
+    </div>
+  </div>
+    // <>
+    // <div>AdminNav</div>
+    // <button onClick={logout}>Logout</button>
+    // </>
   )
 }
 
