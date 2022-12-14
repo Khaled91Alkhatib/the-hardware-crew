608ceb4e02f16eab42f43c3a0c52170bd7a71bfe
Khaled Alkhatib <khaledkhatib91@hotmail.com>
2022-12-14 13:57:44 -0500

    Create component to edit existing products

---
+++ src/components/EditProducts/EditProducts.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import "./EditProducts.scss"
+
+const editProducts = () => {
+  return (
+    <div className='edit-product'>
+      editProducts
+      </div>
+  )
+}
+
+export default editProducts
