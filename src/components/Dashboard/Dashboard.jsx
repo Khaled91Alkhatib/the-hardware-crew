ed2d13c5c34e2b146426902ca509a1e42a63660b
Khaled Alkhatib <khaledkhatib91@hotmail.com>
2023-01-04 23:33:23 -0500

    Remove axios comments as it was use in App.js instead

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -5,33 +5,6 @@ import { ToastContainer } from 'react-toastify';
 import './Dashboard.scss';
 
 const Dashboard = ({ login, register, setRegisterUsername, setRegisterPassword, setUsername, setPassword }) => {
-  // const navigate = useNavigate();
-  // const [registerUsername, setRegisterUsername] = useState("");
-  // const [registerPassword, setRegisterPassword] = useState("");
-
-  // const [username, setUsername] = useState("");
-  // const [password, setPassword] = useState("");
-
-  // const register = () => {
-  //   axios.post('http://localhost:5001/users', { username: registerUsername, password: registerPassword })
-  //     .then((res) => {
-  //       console.log(res);
-  //     });
-  // };
-
-  // const login = () => {
-  //   axios.post('http://localhost:5001/users/login', { username: username, password: password })
-  //     .then((res) => {
-  //       console.log(res);
-  //       if (res.data.rows.length === 0) {
-  //         toast("Invalid Credentials!", { position: "top-right", type: 'success', autoClose: 1500, theme: 'dark' });
-  //       } else {
-  //         navigate("./AddProducts");
-  //       }
-  //     });
-  // };
-
-
 
   return (
     <div className='user-login'>
