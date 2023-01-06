0941d46cdbf023503b45d660cd6e6a817439114a
Khaled Alkhatib <khaledkhatib91@hotmail.com>
2023-01-05 20:46:03 -0500

    Add useEffect so that page starts from top when clicking categories

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -1,10 +1,14 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { NavLink } from 'react-router-dom';
 
 import './Product.scss';
 
 const Product = ({ product }) => {
   // console.log("khaled", product);
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
 
   return (
     <div className='main-in-product'>
