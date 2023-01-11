36b0930178a64da4a666c2faf4aefbeb6cb0d251
Khaled Alkhatib <khaledkhatib91@hotmail.com>
2023-01-11 10:19:58 -0500

    Implement code for responsive design to toggle menu bar for smaller screens

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,7 +1,9 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { NavLink, useNavigate } from 'react-router-dom';
 import logo from '../../site-images/logo.png';
 import GeneralContext from '../../contexts/GeneralContext';
+import { HiMenuAlt4, HiX } from 'react-icons/hi';
+import { motion } from 'framer-motion';
 
 import './Navbar.scss';
 
@@ -17,14 +19,30 @@ const Navbar = () => {
   const clickLogo = () => {
     navigate('/');
   };
+  const [toggle, setToggle] = useState(false);
 
   return (
     <div className='nav-buttons'>
       <img onClick={clickLogo} src={logo} alt='logo' />
-      <div>
+      <div className='collection-names'>
         <button><NavLink className='collection' to="/products/keyboards">Keyboards</NavLink></button>
         <button><NavLink className='collection' to="/products/mice">Mice</NavLink></button>
         <button><NavLink className='collection' to="/products/headsets">Headsets</NavLink></button>
+      </div>
+      <div className='app__navbar-menu'>
+        <HiMenuAlt4 onClick={() => setToggle(true)} />
+        {toggle &&
+          <motion.div
+            whileInView={{ x: [300, 0] }} /* 300px */
+            transition={{ duration: 0.85, ease: 'easeOut' }}
+          >
+            <HiX onClick={() => setToggle(false)} />
+            <ul>
+              <button><NavLink className='collection' to="/products/keyboards">Keyboards</NavLink></button>
+              <button><NavLink className='collection' to="/products/mice">Mice</NavLink></button>
+              <button><NavLink className='collection' to="/products/headsets">Headsets</NavLink></button>
+            </ul>
+          </motion.div>}
       </div>
       <div>
         <button className='cart'><NavLink to="/shoppingcart"><FontAwesomeIcon icon="fa-solid fa-cart-shopping" size='lg' /><span className='badge'>{cart.length}</span></NavLink></button>
